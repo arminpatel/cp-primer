fce34998b63bc754ba77a2f03d2f71254bc0b1e3
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-05-13 23:28:00 +0530

    made responsive

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,24 @@ function App() {
           <h1> CP Primer </h1>
           <div> A Primer course in Competitive Programming </div>
       </div>
-      <article className = "container">
+      <center>
+        <select className="dropdown-mobile" onChange={(e) => {setWeek(e.target.value)}} value={week}>
+          {Object.keys(data).map((wk) => {
+            return (
+              <option key={wk} value={wk}> {data[wk]["title"]} </option>
+            )
+          })
+          }
+        </select>
+      </center>
+      <article className = "container-fluid">
         <aside>
           <nav>
             <ul>
               {Object.keys(data).map((wk) => {
                 return (
-                  <div key={data[wk]["title"]}> 
-                    <li><a data-week={wk} className="sidebar-link" onClick={(e) => {console.log(e.target.dataset.week); setWeek(e.target.dataset.week)}}> {data[wk]["title"]}
+                  <div key={wk}> 
+                    <li><a data-week={wk} className="sidebar-link" onClick={(e) => {setWeek(e.target.dataset.week)}}> {data[wk]["title"]}
                     </a> </li>
                   </div>
                 )
@@ -26,44 +36,46 @@ function App() {
           </nav>
         </aside>
         <section>
-          <table>
-            <thead>
-              <tr>
-                <th> {data[week]["title"]} </th>
-                <th> About </th>
-                <th> Task 0 </th>
-                <th> Task 1 </th>
-                <th> Task 2 </th>
-              </tr>
-            </thead>
+          <figure>
+            <table>
+              <thead>
+                <tr>
+                  <th> {data[week]["title"]} </th>
+                  <th> About </th>
+                  <th> Task 0 </th>
+                  <th> Task 1 </th>
+                  <th> Task 2 </th>
+                </tr>
+              </thead>
 
-            <tbody>
-              { Object.keys(data[week]).map((day) => {
-                  if(day == "title") return;
-                  return (
-                    <tr key = {day}>
-                      { Object.keys(data[week][day]).map((field) => {
-                        if(field.startsWith("task")) {
-                          return (
-                            <td key = {week+day+field}> 
-                              <a href={data[week][day][field]["link"]}>
-                                {data[week][day][field]["text"]}
-                              </a> 
-                            </td>
-                          )
-                        } else {
-                          return (
-                            <td key = {week+day+field}> {data[week][day][field]} </td>
-                          )
+              <tbody>
+                { Object.keys(data[week]).map((day) => {
+                    if(day == "title") return;
+                    return (
+                      <tr key = {day}>
+                        { Object.keys(data[week][day]).map((field) => {
+                          if(field.startsWith("task")) {
+                            return (
+                              <td key = {week+day+field}> 
+                                <a href={data[week][day][field]["link"]}>
+                                  {data[week][day][field]["text"]}
+                                </a> 
+                              </td>
+                            )
+                          } else {
+                            return (
+                              <td key = {week+day+field}> {data[week][day][field]} </td>
+                            )
+                          }
+                        })
                         }
-                      })
-                      }
-                    </tr>
-                  )
-                })
-              } 
-            </tbody>
-          </table>
+                      </tr>
+                    )
+                  })
+                } 
+              </tbody>
+            </table>
+          </figure>
         </section>
       </article>
     </div>
